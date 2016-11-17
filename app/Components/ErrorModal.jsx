94b2269984519bdba047a8734cb314670e09bad7
Sudhin Suresh <ssuresh@goodmethodsglobal.com>
2016-11-17 16:38:46 +0530

    Confusuion commit

--- app/Components/ErrorModal.jsx
+++ app/Components/ErrorModal.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function () {
+    return{
+      title: 'Error'
+    }
+  },
   componentDidMount: function (){
     var modal = new Foundation.Reveal($('#error-modal'))
     modal.open();
